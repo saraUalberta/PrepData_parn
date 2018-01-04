95cd412f2909611dd58a091190d346873f1f5bc8
hejazizo <hejazizo@ualberta.ca>
2018-01-04 17:50:42 +0000

    ForthRun

---
+++ 3068/scripts/recon-all.cmd
@@ -0,0 +1,7 @@
+
+
+#---------------------------------
+# New invocation of recon-all Fri Dec 29 00:28:53 UTC 2017 
+
+ mri_convert /home/ubuntu/sMRI/3068.nii /usr/local/freesurfer/subjects/3068/mri/orig/001.mgz 
+
